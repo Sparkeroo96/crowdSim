b6d22bd38c22181c0f907b5d85ca469a55d3bf53
Philip Sandford <Sandford_Philip@Network.lilly.com>
2019-01-03 13:52:26 +0000

    Update baseObject.py

--- Objects/baseObject.py
+++ Objects/baseObject.py
@@ -6,16 +6,47 @@ Created by Sam Parker
 """
 class BaseObject:
 
-    clipThrough = False
-    xSize = 0
-    ySize = 0
-    coordinates = []
+    envObjectName = ""
+    id = ""
+    coordX = 0
+    coordY = 0
+    angle = 0
+    width = 0
 
-    def __init__(self, coordinates):
-        self.coordinates = coordinates
+    def get_details(self):
+        return ([self.envObjectName, self.id, self.coordX, self.coordY, self.angle, self.width])
+    """Cords"""
+    def get_cords(self):
+        cords = [self.coordX, self.coordY]
+        return cords
 
-    def getCoordinates(self):
-        return self.coordinates
+    """Returns what object it is"""
+    def get_env_object_name(self):
+        return self.envObjectName
 
-    def getClipThrough(self):
-        return self.clipThrough
+    def get_id(self):
+        return self.id
+
+    def get_angle(self):
+        return self.angle
+
+    def get_width(self):
+        return self.width
+
+    def set_env_object_name(self, name):
+        self.envObjectName = name
+
+    def set_id(self, newID):
+        self.id = newID
+
+    def set_coordX(self, coordX):
+        self.coordX = coordX
+
+    def set_coordY(self, coordY):
+        self.coordY = coordY
+
+    def set_angle(self, angle):
+        self.angle = angle
+
+    def set_width(self, width):
+        self.width = width
